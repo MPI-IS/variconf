315afe575238fd347c5a715d76402f9450c8f03c
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-10-20 10:02:32 +0200

    Get version from __init__.py
    I just saw that setuptools now supports the `attr`-function for the
    version also in pyproject.toml.

--- wconf/__init__.py
+++ wconf/__init__.py
@@ -1,12 +1,5 @@
-import importlib.metadata
-
 from .wconf import WConf
 
 __all__ = ("WConf",)
 
-# get version from package metadata (only works if package is installed, otherwise fall
-# back to "unknown".
-try:
-    __version__ = importlib.metadata.version(__package__)
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "unknown"
+__version__ = "1.0.0alpha1"
