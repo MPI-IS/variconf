c2b330bbc9621fb67303185a011d619ab315563f
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-10-20 09:11:24 +0200

    Set __version__ based on package metadata

--- wconf/__init__.py
+++ wconf/__init__.py
@@ -1,3 +1,12 @@
+import importlib.metadata
+
 from .wconf import WConf
 
 __all__ = ("WConf",)
+
+# get version from package metadata (only works if package is installed, otherwise fall
+# back to "unknown".
+try:
+    __version__ = importlib.metadata.version(__package__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
