6afcc2e905287e7b9839e48269e29f9fbb49a4dc
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2022-11-02 16:42:14 +0100

    Remove alpha from version

--- variconf/__init__.py
+++ variconf/__init__.py
@@ -3,4 +3,4 @@ from .errors import UnknownFormatError, UnknownExtensionError
 
 __all__ = ("WConf", "UnknownFormatError", "UnknownExtensionError")
 
-__version__ = "1.0.0alpha1"
+__version__ = "1.0.0"
